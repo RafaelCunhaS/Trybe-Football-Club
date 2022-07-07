6fed4dc4691b914c2cc1afe4a4a4513475a137dc
Rafael Santos <rafaelcs_94@hotmail.com>
2022-07-07 20:55:14 -0300

    feat: :sparkles: creates migration and model for teams

---
+++ app/backend/src/database/models/Team.model.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Match from './Match.model';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+Team.hasMany(Match, { foreignKey: 'id', as: 'matches' });
+
+export default Team;
