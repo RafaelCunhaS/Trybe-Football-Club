7a61a47147eeaeb82d882556610bd8630e7703bf
Rafael Santos <rafaelcs_94@hotmail.com>
2022-07-07 17:08:09 -0300

    fix: :bug: fixes Jwt secret

--- app/backend/src/middlewares/authToken.ts
+++ app/backend/src/middlewares/authToken.ts
@@ -2,17 +2,19 @@ import { NextFunction, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as jwt from 'jsonwebtoken';
 import { JwtPayload } from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 import { RequestUser } from '../interfaces/RequestUser.interface';
 import ErrorHandler from '../utils/ErrorHandler';
 
+dotenv.config();
+
 export default async (req: RequestUser, _res: Response, next: NextFunction) => {
-  const JWT_SECRET = 'senhasupersecreta';
   const token = req.headers.authorization;
 
   if (!token) throw new ErrorHandler(StatusCodes.UNAUTHORIZED, 'Token not found');
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as jwt.Secret) as JwtPayload;
 
     req.user = decoded.data;
 
