55ebab0a6db675c05eaefc1cc91a08244fcfea5d
Rafael Santos <rafaelcs_94@hotmail.com>
2022-07-08 12:49:19 -0300

    test: :white_check_mark: adds test for /teams/:id GET method

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -45,4 +45,28 @@ describe('Tests GET method for /teams', () => {
       expect(chaiHttpResponse.body).to.have.length(3);
     });
   });
+
+  describe('if the request is made on /teams/:id', () => {
+    let chaiHttpResponse: Response;
+    const team = {
+      "id": 5,
+      "teamName": "Cruzeiro"
+    }
+
+    before(() => sinon.stub(Team, 'findByPk').resolves(team as Team));
+
+    after(() => (Team.findByPk as sinon.SinonStub).restore())
+
+    it('it should return the status code 200', async () => {
+      chaiHttpResponse = await chai.request(app).get('/teams/5');
+  
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+    it('the body should return only one team', () => {
+      expect(chaiHttpResponse.body).to.be.an('object');
+    });
+    it('the object must contain the keys "id" and "teamName"', () => {
+      expect(chaiHttpResponse.body).to.have.keys('id', 'teamName');
+    });
+  });
 });
