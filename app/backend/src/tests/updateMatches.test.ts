a93e3f409c277f504fc2e2b44d06c8e215144630
Rafael Santos <rafaelcs_94@hotmail.com>
2022-07-09 16:56:35 -0300

    test: :white_check_mark: adds tests for updateMatches

--- app/backend/src/tests/updateMatches.test.ts
+++ app/backend/src/tests/updateMatches.test.ts
@@ -24,7 +24,29 @@ describe('Tests PATCH method for /matches', () => {
     });
     it('the object must contain the key "message" with the value "Finished"', () => {
       expect(chaiHttpResponse.body).to.have.key('message');
-      expect(chaiHttpResponse.body.message).to.have.key('Finished');
+      expect(chaiHttpResponse.body.message).to.be.equal('Finished');
+    });
+  });
+
+  describe('if the request is made on /matches/:id', () => {
+    let chaiHttpResponse: Response;
+    const insertedData = {
+      "homeTeamGoals": 3,
+      "awayTeamGoals": 1
+    }
+
+    it('it should return the status code 200', async () => {
+      chaiHttpResponse = await chai.request(app).patch('/matches/1')
+        .send(insertedData);
+  
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+    it('the body should return an object', () => {
+      expect(chaiHttpResponse.body).to.be.an('object');
+    });
+    it('the object must contain the key "message" with the value "Updated"', () => {
+      expect(chaiHttpResponse.body).to.have.key('message');
+      expect(chaiHttpResponse.body.message).to.be.equal('Updated');
     });
   });
 });
