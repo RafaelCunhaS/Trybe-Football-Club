8f183679c302b32648c1146bcbaa734a1533e4dc
Rafael Santos <rafaelcs_94@hotmail.com>
2022-07-09 15:31:20 -0300

    test: :white_check_mark: adds test for PATCH method in /matches

---
+++ app/backend/src/tests/updateMatches.ts
@@ -0,0 +1,30 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Tests PATCH method for /matches', () => {
+  describe('if the request is made on /matches/:id/finish', () => {
+    let chaiHttpResponse: Response;
+
+    it('it should return the status code 200', async () => {
+      chaiHttpResponse = await chai.request(app).patch('/matches/1/finish');
+  
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+    it('the body should return an object', () => {
+      expect(chaiHttpResponse.body).to.be.an('object');
+    });
+    it('the object must contain the key "message" with the value "Finished"', () => {
+      expect(chaiHttpResponse.body).to.have.key('message');
+      expect(chaiHttpResponse.body.message).to.have.key('Finished');
+    });
+  });
+});
